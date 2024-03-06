d4bf38526bbcecfe2afc4098684a94ffe4d3acc2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-06 07:33:07 +0430

    useEffect fetch

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -7,16 +7,32 @@ const UseEffectFetchData = () => {
 
   const getUsers = async () => {
     const respons = await fetch(url);
-    const data = respons.json();
-    setUsers(data)
+    const users = await respons.json();
+    setUsers(users);
   }
-  
+
   useEffect(() => {
     getUsers()
-  })
+  }, []);
 
-
-  return <h2>fetch data</h2>;
+  return <>
+      <h3>GitHub Users</h3>
+      <ul className='users'>{
+        users.map((user) => {
+          const { id, avatar_url, html_url, login } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          )
+        })
+      }
+      </ul>
+  </>;
 };
 
 export default UseEffectFetchData;
