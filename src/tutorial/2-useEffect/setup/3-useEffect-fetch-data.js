458b956ac96b990cc0101daff3fcefc8a666a096
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-06 07:19:43 +0430

    useState fetch

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
+  const [users, setUsers] = useState([]);
+  
+
+
   return <h2>fetch data</h2>;
 };
 
