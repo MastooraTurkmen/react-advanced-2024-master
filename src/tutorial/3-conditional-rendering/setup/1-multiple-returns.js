60bea2e27b0a5b28c56d4f08909c13f1c28afe12
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-06 10:00:31 +0430

    is loading true

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
+
+
 const MultipleReturns = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+
   return <h2>multiple returns</h2>;
 };
 
