d9ee917c3f551ccc800bc2dc74ecc121eb9cea0e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-06 08:05:23 +0430

    target blank

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -6,19 +6,9 @@ const UseEffectFetchData = () => {
   const [users, setUsers] = useState([]);
 
   const getUsers = async () => {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      const data = await response.json();
-      if (!Array.isArray(data)) {
-        throw new Error('Invalid data format');
-      }
-      setUsers(data);
-    } catch (error) {
-      console.log(error);
-    }
+    const response = await fetch(url);
+    const data = await response.json();
+    setUsers(data);
   };
 
   useEffect(() => {
@@ -36,7 +26,7 @@ const UseEffectFetchData = () => {
               <img src={avatar_url} alt={login} />
               <div>
                 <h4>{login}</h4>
-                <a href={html_url}>Profile</a>
+                <a target='_blank' href={html_url}>Profile</a>
               </div>
             </li>
           );
