8e490818efabdcf22152e83d8dce02b8af6e41fb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-06 10:30:03 +0430

    conditional rendering- multiple returns

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -5,7 +5,25 @@ const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState("default User")
+  const [user, setUser] = useState("default User");
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => {
+        if (response.status >= 200 && response.status <= 299) {
+          return response.json()
+        } else {
+          setIsLoading(false);
+          setIsError(true)
+          throw new Error(response.statusText)
+        }
+      })
+      .then(data => {
+        const { login } = data;
+        setUser(login)
+        setIsLoading(false)
+      }).catch((error) => console.log(error))
+  }, [])
 
   if (isLoading) {
     return <div>
