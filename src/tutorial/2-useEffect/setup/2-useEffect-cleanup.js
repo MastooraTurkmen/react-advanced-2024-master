720897d84daa6474402e211278d04185e9cbc6c8
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-06 06:36:27 +0430

    useEffect clean up

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,28 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  function resizFunction() {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    console.log("Use Effect Comingggggggggg............");
+    window.addEventListener('resize', resizFunction);
+
+    // return (() => {
+    //   window.removeEventListener('resize', resizFunction);
+    //   console.log("clean up")
+    // })
+  }, [])
+
+  return (
+    <div>
+      <h1>Window Resize</h1>
+      <h1>{size} PX</h1>
+    </div>
+  )
 };
 
 export default UseEffectCleanup;
