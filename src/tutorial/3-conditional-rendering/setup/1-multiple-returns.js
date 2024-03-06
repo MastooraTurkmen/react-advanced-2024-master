5562ed1d777f197d937f34a7feb3218de2f17692
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-06 10:01:39 +0430

    user

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -5,6 +5,7 @@ const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default User")
 
 
   return <h2>multiple returns</h2>;
