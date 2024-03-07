03c41703eedf1f8b107b5492e7d00032640cc8a2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-07 06:21:06 +0430

    control input

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -13,8 +13,16 @@ const ControlledInputs = () => {
     const value = e.target.value;
     setPerson({ ...person, [name]: value });
   };
-  
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (person.firstName && person.email && person.age) {
+      const newPerson = { ...person, id: new Date().getTime().toString() };
+      setPeople([...people, newPerson]);
+      setPerson({ firstName: '', email: '', age: '' });
+    }
+  };
+
   return (
     <>
       <article className='form'>
