8e6d34e471404ed1f1acbb58845751355bb78679
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-07 06:20:04 +0430

    multiple input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide'
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs'
 
 function App() {
   return (
