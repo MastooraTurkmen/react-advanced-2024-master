4892cf189326102fc2d2fb303b2092115feca9a0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-15 08:47:09 +0430

    handle submit

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -8,10 +8,22 @@ const Index = () => {
   const [name, setName] = useState('');
   const [people, setPeople] = useState(data);
 
+  const handleSumbit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <>
       {showModal && <Modal />}
-      <form className='form'></form>
+      <form onSubmit={handleSumbit} className='form'>
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      </form>
     </>
   )
 };
