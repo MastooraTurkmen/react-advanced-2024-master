fcc272397cf02bf81f7538aecf940732eef6bf80
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-22 19:15:16 +0430

    Prop drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,51 +1,46 @@
-import React, { useContext, useState } from 'react';
-import { data } from '../../../data.js'
-
+import React, { useState } from 'react';
+import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
-const ContextPerson = React.createContext()
-
 const PropDrilling = () => {
   const [people, setPeople] = useState(data);
-
-  const removeItem = (id) => {
+  const removePerson = (id) => {
     setPeople((people) => {
-      return people.filter((person) => person.id !== id)
+      return people.filter((person) => person.id !== id);
     });
   };
-
   return (
-    <ContextPerson.Provider value={{ removeItem, people }}>
+    <section>
       <h3>prop drilling</h3>
-      <List />
-    </ContextPerson.Provider>
-  )
+      <List people={people} removePerson={removePerson} />
+    </section>
+  );
 };
 
-const List = () => {
-  const mainData = useContext(ContextPerson)
-
+const List = ({ people, removePerson }) => {
   return (
     <>
-      {mainData.people.map((person) => {
-        return <SinglePerson key={person.id} {...person} />
-      })
-      }
+      {people.map((person) => {
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+            removePerson={removePerson}
+          />
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-
-const SinglePerson = ({ id, name }) => {
-  const { removeItem } = useContext(ContextPerson)
-
+const SinglePerson = ({ id, name, removePerson }) => {
   return (
     <div className='item'>
       <h4>{name}</h4>
-      <button onClick={() => removeItem(id)}>remove</button>
+      <button onClick={() => removePerson(id)}>remove</button>
     </div>
-  )
-}
+  );
+};
 
 export default PropDrilling;
