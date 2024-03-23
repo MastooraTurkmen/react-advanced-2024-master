eafee917bcf051dc269381e06948c263dd628d3e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-23 09:39:03 +0430

    fetch example

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './2-useFetch'
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
