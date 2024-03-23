13efe6e0c4b0a9123d2a4af4eb28da90c74c052c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-23 09:26:06 +0430

    fetch example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/7-prop-drilling/setup/1-prop-drilling'
+import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example'
 
 function App() {
   return (
