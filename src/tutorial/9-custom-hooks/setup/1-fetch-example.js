5f72f505750cc4e92a560975f9922ee7a5a6d189
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 10:58:41 +0430

    useCallback

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useFetch } from './2-useFetch'
 
 const url = 'https://course-api.com/javascript-store-products'
@@ -7,17 +7,17 @@ const Example = () => {
   const [loading, setLoading] = useState(true)
   const [products, setProducts] = useState([])
 
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const response = await fetch(url)
     const products = await response.json()
     setProducts(products)
     setLoading(false)
-  }
+  }, [url])
 
   useEffect(() => {
     getProducts()
-  }, [url])
-  console.log(products)
+  }, [url, products])
+
   return (
     <div>
       <h2>{loading ? 'loading...' : 'data'}</h2>
